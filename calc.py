e87bd377de6593ad851ddecd26647e305e4fc596
acereca <tigerauge96@live.de>
2017-05-02 20:26:18 +0200

    change plotting to be handled by hexes, add hexmap-class for paths

--- calc.py
+++ calc.py
@@ -6,6 +6,7 @@ import json
 from hexagon import *
 
 settlements = []
+hexmap = hexmap()
 
 #hexes
 data2 = pd.read_csv('testmap.csv', sep=',')
@@ -21,27 +22,30 @@ for i,e in data.iterrows():
         if e[side] == True:
             connected_sides.append(int(side))
 
-    if len(connected_sides) == 1:
-        corner_pos_1 = active_hex.hex_corner(connected_sides[0])
-        corner_pos_2 = active_hex.hex_corner(connected_sides[0]+1)
-        s = data2.loc[(data2['q'] == e['q']) & (data2['r'] == e['r'])]
-        if (s.shape[0]) == 1:
-            x_mod = (active_hex.get_x_pos() + (corner_pos_1[0]+corner_pos_2[0])/2)/2
-            y_mod = (active_hex.get_y_pos() + (corner_pos_1[1]+corner_pos_2[1])/2)/2
+    if len(connected_sides) == 1 or len(connected_sides) >= 3:
 
-        else:
-            x_mod = active_hex.get_x_pos()
-            y_mod = active_hex.get_y_pos()
+        for cs in connected_sides:
+            #print(active_hex.hex_side(cs))
+            s = data2.loc[(data2['q'] == e['q']) & (data2['r'] == e['r'])]
 
-        plt.plot(
-            [x_mod, active_hex.hex_side(connected_sides[0])[0]],
-            [y_mod, active_hex.hex_side(connected_sides[0])[1]],
-            lw = 5,
-            c = '#777777',
-            zorder = 1
-        )
+            if (s.shape[0]) == 1:
+                x_mod = (active_hex.get_x_pos() + active_hex.hex_side(cs)[0]) / 2
+                y_mod = (active_hex.get_y_pos() + active_hex.hex_side(cs)[1]) / 2
+
+            else:
+                x_mod = active_hex.get_x_pos()
+                y_mod = active_hex.get_y_pos()
+
+            # plt.plot(
+            #     [x_mod, active_hex.hex_side(cs)[0]],
+            #     [y_mod, active_hex.hex_side(cs)[1]],
+            #     lw = 5,
+            #     c = '#777777',
+            #     zorder = 1
+            # )
+
     if len(connected_sides) == 2:
-        print(connected_sides)
+        #print(connected_sides)
 
         side1_pos = active_hex.hex_side(connected_sides[0])
         side2_pos = active_hex.hex_side(connected_sides[1])
@@ -52,51 +56,37 @@ for i,e in data.iterrows():
             (side1_pos[1]+side2_pos[1]+midpoint[1])/3
         ]
 
-        plt.plot(
-            [side1_pos[0], tripoint[0], side2_pos[0]],
-            [side1_pos[1], tripoint[1], side2_pos[1]],
-            lw = 5,
-            c = '#777777',
-            zorder = 1
-        )
+        # plt.plot(
+        #     [side1_pos[0], tripoint[0], side2_pos[0]],
+        #     [side1_pos[1], tripoint[1], side2_pos[1]],
+        #     lw = 5,
+        #     c = '#777777',
+        #     zorder = 1
+        # )
 
 
-qlim = [0,0]
-rlim = [0,0]
 for i,e in data2.iterrows():
-    hgrid = hex(e['q'],e['r'], c=('plains' if e['c'] == '' else e['c']))
-    #print(e)
-    plt.gca().add_patch(hgrid.to_poly())
+    hgrid = hex(
+        e['q'],
+        e['r'],
+        c=('plains' if e['c'] == '' else e['c']),
+        settlement=e['settlement'],
+        name=e['name'],
+        road=(False if e['road'] != True else True)
+    )
+    # to be removed in future
+    hgrid.plot()
+    hexmap.add(hgrid)
 
+xy = np.array(hexmap.get_road_pos()).T
+sxy = np.empty(xy.shape)
+sxy[0] = xy[0]
+sxy[xy.shape[0]-1] = xy[xy.shape[0]-1]
+for i in np.arange(1,xy.shape[0]-1):
+     sxy[i] = (xy[i-1]+xy[i]+xy[i+1])/3
 
-    # settlements
-    if isinstance(e['settlement'], str):
-        t = e['settlement']
-        print(t)
-        plt.gca().add_patch(
-            plt.Circle(
-                (hgrid.get_x_pos(), hgrid.get_y_pos()),
-                settlement_types[t]['size']/2,
-                fc=settlement_types[t]['fc'],
-                lw=4,
-                aa=True,
-                fill = True,
-                ec=settlement_types[t]['ec'],
-                zorder = 2
-            )
-        )
-        plt.annotate(
-            e['name'],
-            xy = (hgrid.get_x_pos(), hgrid.get_y_pos()),
-            fontsize = 14,
-            xytext=(0,1),
-            ha = 'center',
-            va = 'center',
-            bbox=dict(boxstyle="round", fc="#ffffff55", ec='#ffffff00')
-        )
-        settlements.append(hgrid)
-
-
+print(sxy, xy)
+plt.plot(sxy[:,0],sxy[:,1], c='r', zorder=1)
 
 
 plt.axis('equal') # fit to plotted hexes
--- hexagon.py
+++ hexagon.py
@@ -2,20 +2,21 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 terrain_colors = {
-    'snow':'#ece7f2',
-    'ice':'#e5f5f9',
-    'plains':'#a8ddb5',
-    'forest':'#669973',
-    'mountain':'#8d8d8d',
-    'desert':'#ffffcc',
-    'fields':'#8c9966',
-    'swamp':'#998066'
+    'none':'#000000a0',
+    'snow':'#ece7f2a0',
+    'ice':'#e5f5f9a0',
+    'plains':'#a8ddb5a0',
+    'forest':'#669973a0',
+    'mountain':'#8d8d8da0',
+    'desert':'#ffffcca0',
+    'fields':'#8c9966a0',
+    'swamp':'#998066a0'
 }
 
 settlement_types = {
     'city':{
         'size': 1,
-        'ec': '#000000ff',
+        'ec': '#00000070',
         'fc': '#8d8d8d'
     },
     'village':{
@@ -26,8 +27,14 @@ settlement_types = {
 }
 
 class hex(object):
-    def __init__(self, x1=0, x2=0, s=1, c='plains'):
-        self.x1, self.x2, self.s, self.c = x1,x2,s,c
+    def __init__(self, x1, x2, s=1, c='plains', settlement=None, name=None, road=False):
+        self.x1 = x1
+        self.x2 = x2
+        self.s = s
+        self.c = c
+        self.settlement = settlement
+        self.name = name
+        self.road = road
 
     def get_x_pos(self):
         return (self.x1+self.x2/2) * np.sqrt(3) * self.s
@@ -35,8 +42,14 @@ class hex(object):
     def get_y_pos(self):
         return self.x2 * 3 / 2 * self.s
 
+    def get_pos(self):
+        return (self.get_x_pos(), self.get_y_pos())
+
+    def get_coord(self):
+        return (self.x1, self.x2)
+
     def hex_corner(self, i):
-        angle_deg = 60 * i +30
+        angle_deg = 60 * i + 30
         angle_rad = np.pi / 180 * angle_deg
         return [
             self.get_x_pos() + self.s * np.cos(angle_rad),
@@ -45,8 +58,11 @@ class hex(object):
 
     def hex_side(self, i):
         s1 = self.hex_corner(i)
-        s2 = self.hex_corner(i+1)
-        return [(s1[0]+s2[0])/2, (s1[1]+s2[1])/2]
+        s2 = self.hex_corner(i + 1)
+        return [
+            (s1[0]+s2[0])/2,
+            (s1[1]+s2[1])/2
+        ]
 
     def to_poly_list(self):
         pts = [self.hex_corner(i) for i in range(0,6)]
@@ -68,3 +84,60 @@ class hex(object):
                     +abs(self.x2 - self.x2)
                     +abs(-self.x1-self.x2+hex.x1+hex.x2)
                 )/2
+
+    def plot(self):
+        plt.gca().add_patch(self.to_poly())
+        plt.annotate(
+            "{:2},{:2}".format(self.x1,self.x2),
+            xy = (self.get_x_pos(), self.get_y_pos()),
+            ha = 'center',
+            va = 'center',
+            color = '#00000020'
+        )
+
+        # settlements
+        if isinstance(self.settlement, str):
+            plt.gca().add_patch(
+                plt.Circle(
+                    (self.get_x_pos(), self.get_y_pos()),
+                    settlement_types[self.settlement]['size']/2,
+                    fc=settlement_types[self.settlement]['fc'],
+                    lw=4,
+                    aa=True,
+                    fill = True,
+                    ec=settlement_types[self.settlement]['ec'],
+                    zorder = 2
+                )
+            )
+            plt.annotate(
+                self.name,
+                xy = (self.get_x_pos(), self.get_y_pos()),
+                fontsize = 14,
+                xytext=(0,1),
+                ha = 'center',
+                va = 'center',
+                bbox=dict(boxstyle="round", fc="#ffffff55", ec='#ffffff00')
+            )
+
+
+class hexmap(object):
+    def __init__(self, hlist=[]):
+        self.hexlist = hlist
+        self.roadlist = []
+
+    def add(self, gridpoint: hex):
+        self.hexlist.append(gridpoint)
+        if gridpoint.road:
+            pos = gridpoint.get_pos()
+            coord = gridpoint.get_coord()
+            self.roadlist.append([coord, pos])
+            #print("added road at pos {}, with coordinates {}".format(pos,coord))
+
+    def get_road_pos(self):
+        x = []
+        y = []
+        for e in self.roadlist:
+            x.append(e[1][0])
+            y.append(e[1][1])
+
+        return (x,y)
